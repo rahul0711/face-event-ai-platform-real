3df467d929857736be46d6061e22414eef193786
Sonu Santhosh <sonusanthosh972@gmail.com>
2026-02-14 16:14:38 +0530

    cors again

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ app.use(cookieParser())
 checkDBConnection()
 app.use(
   cors({
-    origin: ["*","http://localhost:5173"],
+    origin: ["http://localhost:5173",
+  "http://127.0.0.1:5173"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
